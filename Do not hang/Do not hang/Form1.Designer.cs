f08f079497b72e56c213cfc05f63124f18ab53fc
PudgeSlarkov <80644969+PudgeSlarkov@users.noreply.github.com>
2021-11-17 18:44:42 +0300

    Add files via upload

---
+++ Do not hang/Do not hang/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Do_not_hang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(382, 330);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(400, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(278, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Начать игру";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(400, 264);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(278, 39);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(400, 309);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(278, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Дать ответ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(400, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 161);
+            this.label1.TabIndex = 5;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(505, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 360);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Do not hang/Do not hang/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Do_not_hang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string text;
+        public char[] word;
+        public int hp;
+        public int hptrue;
+        List<string> rndword = (new string[] {"велосипед", "стул", "стол", "автомобиль", "мама", "папа", "окно", "дорога", "небо", "фотоапарат", "телефон", "авто", "университет", "гора", "брат", "семья", "работа", "алфовит", "дерево", "самолет" }).ToList();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string v = rndword[new Random().Next(rndword.Count)];
+            char[] wordv = v.ToCharArray();
+            label2.Text = v;
+            text = label2.Text; 
+            hp = 0;
+            label1.Text = null;         
+            word = new char[v.Length];
+            for (int i = 0; i < text.Length; i++)
+            {
+                label1.Text += '.';
+                word[i] = '.';
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool words = false;
+            int j = 0;
+            if (textBox2.Text.Length == 1) 
+            {
+                for (int i = 0; i < text.Length; i++)
+                {
+                    if (textBox2.Text[0] == text[i])
+                    {
+                        words = true;
+                        hptrue++;
+                        word[i] = text[i];
+                        label1.Text = null;
+                    }
+                }
+                if (words == true)
+                {
+                    for (int i=0; i< text.Length; i++)
+                    {
+                        label1.Text += word[i];
+                    }
+                    if (hptrue == text.Length)
+                    {
+                        MessageBox.Show("Ты победил!");
+                    }
+                }
+                else
+                {
+                    hp++;
+                    string hpbuff = hp.ToString() + ".png";
+                    pictureBox1.Image = new Bitmap(hpbuff);
+                    if (hp == 10)
+                    {
+                        MessageBox.Show("Ты проиграл!");
+                        label1.Text = null;
+                        label1.Text = text;
+                    }
+                }
+               
+            }
+            else if (textBox2.Text.Length > 1) 
+            {
+                if (textBox2.Text == text)
+                {
+                    label1.Text = null;
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        label1.Text += text[i];
+                    }
+                    MessageBox.Show("Ты победил!");
+                }
+                else
+                {
+                    hp = 10;
+                    string hpbuff = hp.ToString() + ".png";
+                    pictureBox1.Image = new Bitmap(hpbuff);
+                    MessageBox.Show("Ты проиграл!");
+                    label1.Text = null;
+                    label1.Text = text;
+                }
+            }
+            textBox2.Text = null;
+        }
+    }
+}
